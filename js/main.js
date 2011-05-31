d3076e0464e2169c3457ab4d14ddcc2540141225
Paul Straw <paulstraw@paulstraw.com>
2011-05-31 02:13:37 -0700

    Some minor, unexciting website changes.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
+	function positionFooter() {
+		var main = $('#main'),
+			footer = $('footer');
 
+		main.css('paddingBottom', footer.outerHeight());
+		footer.css('margin-top', - footer.outerHeight());
+	}
+
+	positionFooter();
+
+	$(window).bind('resize', positionFooter);
 });
 
 $(window).load(function() {
 	$('#slider').nivoSlider({
-		controlNav: false
+		controlNav: false,
+		directionNav: false
 	});
 });
